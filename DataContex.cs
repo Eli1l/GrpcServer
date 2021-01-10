62ab702c88d87812f89dc763f8ad2e9f514d2dbc
Eli1l <70685269+Eli1l@users.noreply.github.com>
2021-01-10 20:55:53 +0600

    Add files via upload

---
+++ DataContex.cs
@@ -0,0 +1,21 @@
+﻿using GrpcSE1Server.Models;
+using LinqToDB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcSE1Server.Data
+{
+    public class DataContex : DbContex
+    {
+        public DataContex(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Catrgories { get; set; }
+        public DbSet<GameProduct> Products { get; set; }
+    }
+}
